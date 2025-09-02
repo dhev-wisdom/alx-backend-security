a4a338e3eac0c5f195f0bfc3d6369800ae986892
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-09-02 01:27:17 +0100

    ipinfo

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,9 +1,21 @@
 from ipware import get_client_ip
 from .models import RequestLog, BlockedIP
 from django.http import HttpResponseForbidden
+from django.core.cache import cache
 import logging
+import ipinfo
+import environ
+import os
+from pathlib import Path
+env = environ.Env()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR / ".env")
 
 logger = logging.getLogger(__name__)
+
+IPINFO_TOKEN = env("IPINFO_TOKEN")
+ipinfo_handler = ipinfo.getHandler(IPINFO_TOKEN)
 
 class IPLoggingMiddleWare:
     def __init__(self, get_response):
@@ -17,10 +29,33 @@ class IPLoggingMiddleWare:
         if BlockedIP.objects.filter(ip_address=ip).exists():
             logger.warning(f"Blocked request from ip {ip}")
             return HttpResponseForbidden("Your IP has been blocked!")
+        
+        cache_key = f"geo_{ip}"
+        geo_data = cache.get(cache_key)
 
-        RequestLog.objects.create(ip_address=ip, path=request.path)
+        if not geo_data:
+            try:
+                details = ipinfo_handler.getDetails(ip)
+                geo_data = {
+                    "country": details.country_name if hasattr(details, "country_name") else None,
+                    "city": details.city if hasattr(details, "city") else None
+                }
+                cache.set(cache_key, geo_data, timeout=60*60*24)
+            except Exception as e:
+                logger.error(f"Geolocation lookup failed for {ip}: {e}")
+                geo_data = {
+                    "country": None,
+                    "city": None
+                }
 
-        logger.info(f"IP: {ip}, Path: {request.path}")
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=request.path,
+            country=geo_data.get("country"),
+            city=geo_data.get("city")
+        )
+
+        logger.info(f"Request from {ip} ({geo_data.get("country")}, {geo_data.get("city")}) to {request.path}")
 
         return self.get_response(request)
     
--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -7,10 +7,13 @@ class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=200)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return f"{self.ip_address} - {self.path} - {self.id}"
     
+
 class BlockedIP(models.Model):
     """Blocked IP Model"""
     ip_address = models.GenericIPAddressField()
